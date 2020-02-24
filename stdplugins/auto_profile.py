22b56984bf9cc09fb5e251fe68218b4931b41a76
sophia0708 <61420560+sophia0708@users.noreply.github.com>
2020-02-24 16:50:07 +0000

    Add files via upload

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} | Call Me StolenFlash | ⌚️ {HM}"
+        bio = f"📅 {DMY} | 🎃🎃🎃 | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM}|StolenFlash|📅{DM}"
+        name = f"⌚{HM}|☆꧁༒☬🤖🐰ᵏєᵛ丨Ň🐰🤖☬༒꧂☆|📅{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
